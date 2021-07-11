dd720ffbac46a59ddc33a023c351c94ca36ebb4e
Chris Shenkan <cshenkan7@gmail.com>
2021-07-11 16:07:34 -0400

    finished rustlings exercises

---
+++ exercises/conversions/from_str.rs
@@ -0,0 +1,136 @@
+// This does practically the same thing that TryFrom<&str> does.
+// Additionally, upon implementing FromStr, you can use the `parse` method
+// on strings to generate an object of the implementor type.
+// You can read more about it at https://doc.rust-lang.org/std/str/trait.FromStr.html
+use std::error;
+use std::str::FromStr;
+use std::fmt;
+
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: usize,
+}
+
+#[derive(Debug)]
+struct MyError(String);
+
+impl fmt::Display for MyError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "There is an error: {}", self.0)
+    }
+}
+
+impl error::Error for MyError {}
+
+
+// Steps:
+// 1. If the length of the provided string is 0, an error should be returned
+// 2. Split the given string on the commas present in it
+// 3. Only 2 elements should be returned from the split, otherwise return an error
+// 4. Extract the first element from the split operation and use it as the name
+// 5. Extract the other element from the split operation and parse it into a `usize` as the age
+//    with something like `"4".parse::<usize>()`
+// 5. If while extracting the name and the age something goes wrong, an error should be returned
+// If everything goes well, then return a Result of a Person object
+
+impl FromStr for Person {
+    type Err = Box<dyn error::Error>;
+    fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(Box::new(MyError("String cannot have length 0".into())));
+        }
+        let v = s.split(",").collect::<Vec<&str>>();
+        if v.len() != 2 {
+            return Err(Box::new(MyError("String must be a comma seperated name and age".into())));
+        } else if v[0].is_empty() {
+            return Err(Box::new(MyError("Name cannot be empty".into())));
+        } else if v[1].is_empty() {
+            return Err(Box::new(MyError("Age cannot be empty".into())));
+        }
+        Ok(Person { 
+            name: v[0].to_string(),
+            age: v[1].parse::<usize>()?,
+        })
+    }
+}
+
+/* a more rustic solution
+impl FromStr for Person {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            Err("invalid length".to_string())
+        } else {
+            let v: Vec<&str> = s.split(',').collect();
+            let age = v[1].parse::<usize>();
+            if let Ok(age) = age {
+                Ok(Person{name: String::from(v[0]),age: age})
+            } else {
+                Err("invalid age".to_string())
+            }
+        }
+    }
+}*/
+
+fn main() {
+    let p = "Mark,20".parse::<Person>().unwrap();
+    println!("{:?}", p);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty_input() {
+        assert!("".parse::<Person>().is_err());
+    }
+    #[test]
+    fn good_input() {
+        let p = "John,32".parse::<Person>();
+        assert!(p.is_ok());
+        let p = p.unwrap();
+        assert_eq!(p.name, "John");
+        assert_eq!(p.age, 32);
+    }
+    #[test]
+    fn missing_age() {
+        assert!("John,".parse::<Person>().is_err());
+    }
+
+    #[test]
+    fn invalid_age() {
+        assert!("John,twenty".parse::<Person>().is_err());
+    }
+
+    #[test]
+    fn missing_comma_and_age() {
+        assert!("John".parse::<Person>().is_err());
+    }
+
+    #[test]
+    fn missing_name() {
+        assert!(",1".parse::<Person>().is_err());
+    }
+
+    #[test]
+    fn missing_name_and_age() {
+        assert!(",".parse::<Person>().is_err());
+    }
+
+    #[test]
+    fn missing_name_and_invalid_age() {
+        assert!(",one".parse::<Person>().is_err());
+    }
+
+    #[test]
+    fn trailing_comma() {
+        assert!("John,32,".parse::<Person>().is_err());
+    }
+
+    #[test]
+    fn trailing_comma_and_some_string() {
+        assert!("John,32,man".parse::<Person>().is_err());
+    }
+}
